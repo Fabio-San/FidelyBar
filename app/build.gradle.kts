83bdf8a2bebf4b277f863fe4be26cc0df86da419
Fabio-San <fabietto089@gmail.com>
2026-09-11 16:43:32 +0200

    feat: espanso catalogo enti (Risparmio Casa e ~28 catene note) v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.card.fidelybar"
         minSdk = 28
         targetSdk = 37
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10100
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
